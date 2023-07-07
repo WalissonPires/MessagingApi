d7ed1045c5d39a16ec8a8790013edfd4dadc816e
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2023-07-07 16:49:49 -0300

    Allow register functions/values in di

--- src/common/di-container/index.ts
+++ src/common/di-container/index.ts
@@ -1,23 +1,44 @@
 import { diContainer } from "@fastify/awilix";
-import { asClass, Lifetime }from "awilix";
+import { asClass, asFunction, Lifetime } from "awilix";
 
 export abstract class DIContainer {
 
-    public static addSingleton(classCtor: any, name: string) {
+    public static addSingleton(classCtor: any, name: string, type: ServiceType = ServiceType.Class) {
+
+        const as = DIContainer.getRegisterFunction(type);
 
         diContainer.register({
-            [name]: asClass(classCtor, {
+            [name]: as(classCtor, {
                 lifetime: Lifetime.SINGLETON
             })
         });
     }
 
-    public static addScoped(classCtor: any, name: string) {
+    public static addScoped(classCtor: any, name: string, type: ServiceType = ServiceType.Class) {
+
+        const as = DIContainer.getRegisterFunction(type);
 
         diContainer.register({
-            [name]: asClass(classCtor, {
+            [name]: as(classCtor, {
                 lifetime: Lifetime.SCOPED
             })
         });
     }
+
+    private static getRegisterFunction(type: ServiceType) {
+
+        const map = {
+            [ServiceType.Class]: asClass,
+            [ServiceType.Function]: asFunction,
+            [ServiceType.Value]: asClass
+        };
+
+        return map[type];
+    }
+}
+
+export enum ServiceType {
+    Class = 1,
+    Function = 2,
+    Value = 3
 }

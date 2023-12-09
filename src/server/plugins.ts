bcc85e980953b552f8ba6428eb5f924ae61d4f36
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2023-12-09 12:45:40 -0300

    Enable CORS

--- src/server/plugins.ts
+++ src/server/plugins.ts
@@ -1,12 +1,17 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { fastifyAwilixPlugin } from "@fastify/awilix";
 import jwt from "@fastify/jwt";
+import cors from '@fastify/cors';
 import { AppConfig } from "../common/config";
 import { AppServer } from "./base";
 import { AppError } from "../common/errors/app-error";
 import { UserAuthenticate } from "../domains/auth/entities";
 
 export default function registerPlugins(app: AppServer, config: AppConfig) {
+
+    app.register(cors, {
+        origin: '*'
+    });
 
     app.register(fastifyAwilixPlugin, { disposeOnClose: true, disposeOnResponse: true });
 
